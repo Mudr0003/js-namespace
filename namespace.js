02ba8659e85cf8385235bda731bc45f246e04f1e
Mudr0003 <32398069+Mudr0003@users.noreply.github.com>
2017-10-15 14:43:35 -0400

    first go of assignment

---
+++ namespace.js
@@ -0,0 +1,34 @@
+var MUDR0003 = {
+   
+    
+   
+    
+    init: function () {
+        console.log("script in MUDR0003 called");
+        
+        var div = document.createElement("div");
+        
+        div.className = "box";
+        div.textContent = "MUDR00003";
+        
+        document.getElementById("boxes").appendChild(div);
+   
+        div.addEventListener("click", doClick);
+        div.addEventListener("mouseover", doHighlight);
+        div.addEventListener("mouseout", doHighLight);
+        
+        function doHighLight(ev) {
+          this.classList.toggle("highlight");
+        }
+        
+        function doClick(ev) {          
+            this.style.backgroundColor = "blue";
+            this.style.borderColor = "red";
+        }
+        
+    }
+        
+        
+}
+
+
